3315d1868e8618753cb038f0f64442c980115569
atokad5 <kenny.krosky@gmail.com>
2020-03-14 22:05:16 -0700

    🛠

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ let hasMounted = true;
 
 
 function GenerateUser() {
-  let code = '';
   if(window.location.search && cookies.get('has_access') !== true) {
-    code = window.location.search.split('=')[1];
-    const useData = FetchData('http://localhost:5000/?code='+code);
+    const useData = FetchData('http://localhost:5000/?code='+window.location.search.split('=')[1]);
     if(useData) {
       cookies.set('has_access', true)
       cookies.set('access_token', useData.access_token)
@@ -22,25 +20,61 @@ function GenerateUser() {
   }
 }
 
-
+function removeCookies() {
+  console.log('hi')
+  cookies.remove('access_token')
+  cookies.remove('refresh_token')
+  cookies.set('has_access', false)
+  setTimeout(() => window.location.replace('./'))
+}
 
 function Reading() {
   GenerateUser();
-  console.log('here',cookies.getAll())
   const startDate = moment().subtract(90, 'days').format("Y-MM-DDTkk:mm:ss")
-  const nowDate = moment().format("Y-MM-DDTkk:mm:ss")
+  const nowDate = moment().format("Y-MM-DDTkk:mm:ss");
+  
 
-
-      return(
+  if(hasMounted && cookies.get('access_token') !== undefined) {
+     const readings = FetchData(
+      `http://localhost:5000/get-data?access_token=${cookies.get('access_token')}&refresh_token=${cookies.get('refresh_token')}&dateFrom=${startDate}&dateNow=${nowDate}`
+      )
+      if(readings) {
+        console.log(readings)
+        hasMounted = false;
+      }
+  
+  
+  } else {
+    return(
+      <div>
+        <p>loading...</p>
         <div>
-          <p>loading...</p>
-          <div>
-            <a href="https://api.dexcom.com/v2/oauth2/login?client_id=Gm704rNUXZdRLy2SkbMvSA6ansXnIk1H&redirect_uri=http://localhost:3000/&response_type=code&scope=offline_access">Login</a>
-          </div>
+          <a href="https://api.dexcom.com/v2/oauth2/login?client_id=Gm704rNUXZdRLy2SkbMvSA6ansXnIk1H&redirect_uri=http://localhost:3000/&response_type=code&scope=offline_access">Login</a>
         </div>
-      )      
+      </div>
+    )   
+    
+  }
 
-        
+  if(cookies.get('has_access')) {
+    return (
+      <div>data available
+
+        <button onClick={removeCookies}>Log out</button>
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <p>loading...</p>
+        <div>
+          <a href="https://api.dexcom.com/v2/oauth2/login?client_id=Gm704rNUXZdRLy2SkbMvSA6ansXnIk1H&redirect_uri=http://localhost:3000/&response_type=code&scope=offline_access">Login</a>
+        </div>
+      </div>
+    )      
+
+  }    
+      
     
   }
   
