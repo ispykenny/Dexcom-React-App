a46c541995cb5f79a9f6cbaf8c62b222c868af69
ispykenny <kenny.krosky@gmail.com>
2020-03-14 20:55:30 -0700

    updates to generating user

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import React from 'react';
 import Cookies from 'universal-cookie';
 import FetchData from './Utils/FetchData';
+import moment from 'moment';
 const cookies = new Cookies();
+let hasMounted = true;
+
+
 
 
 function GenerateUser() {
   let code = '';
-  if(window.location.search) {
+  if(window.location.search && cookies.get('has_access') !== true) {
     code = window.location.search.split('=')[1];
-    if(cookies.get('access_token')) return;
     const useData = FetchData('http://localhost:5000/?code='+code);
     if(useData) {
-      cookies.set('access_token')
-      cookies.set('refresh_token')
+      cookies.set('has_access', true)
+      cookies.set('access_token', useData.access_token)
+      cookies.set('refresh_token', useData.refresh_token)
+      setTimeout(() => window.location.replace('./'))
     }
   }
-  
-  
 }
 
 
 
 function Reading() {
-  const readings = FetchData(
-    `http://localhost:5000/get-data?access_token=${cookies.get('access_token')}&refresh_token=${cookies.get('refresh_token')}`
-    )
-  return(
-    <div>
-      <div>
-        <a href="https://api.dexcom.com/v2/oauth2/login?client_id=Gm704rNUXZdRLy2SkbMvSA6ansXnIk1H&redirect_uri=http://localhost:3000/&response_type=code&scope=offline_access">Login</a>
-      </div>
-    </div>
-  )
-}
+  GenerateUser();
+  console.log('here',cookies.getAll())
+  const startDate = moment().subtract(90, 'days').format("Y-MM-DDTkk:mm:ss")
+  const nowDate = moment().format("Y-MM-DDTkk:mm:ss")
+
+
+      return(
+        <div>
+          <p>loading...</p>
+          <div>
+            <a href="https://api.dexcom.com/v2/oauth2/login?client_id=Gm704rNUXZdRLy2SkbMvSA6ansXnIk1H&redirect_uri=http://localhost:3000/&response_type=code&scope=offline_access">Login</a>
+          </div>
+        </div>
+      )      
+
+        
+    
+  }
+  
+    
+
+  
+
+  
+
 
 
 export default Reading;
